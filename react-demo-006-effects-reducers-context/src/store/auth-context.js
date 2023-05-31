0b58db74a439b30d74c9f7041d228015590469cf
Abhishek <abhishekguptanits@gmail.com>
2023-05-31 08:56:57 +0530

    initial commit

---
+++ react-demo-006-effects-reducers-context/src/store/auth-context.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', 1);
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+// AuthContext -> An object which contains a component
+
+export default AuthContext;
