5203502f6f707c0960a8e1b9daff45a2b13711eb
Abhishek <abhishekguptanits@gmail.com>
2023-06-02 08:10:07 +0530

    initial commit

---
+++ react-demo-011-custom-hook-with-backend-service/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const requestCofig = {
+      url: "https://react-http-backend-16718-default-rtdb.firebaseio.com/tasks.json"
+    }; 
+
+    const transformTasks = (data) => {
+      const loadedTasks = [];
+  
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    };
+    fetchTasks(requestCofig, transformTasks);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ react-demo-011-custom-hook-with-backend-service/src/components/NewTask/NewTask.js
@@ -0,0 +1,36 @@
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = async (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    const requestCofig = {
+      url: "https://react-http-backend-16718-default-rtdb.firebaseio.com/tasks.json",
+      method: "POST",
+      body: { text: taskText },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    sendTaskRequest(requestCofig, createTask.bind(null, taskText));
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
