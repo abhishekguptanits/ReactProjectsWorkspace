d24338f1ef3dcc8d9b7177a4993315009e1d56b5
Abhishek <abhishekguptanits@gmail.com>
2023-06-03 13:20:35 +0530

    initial commit

---
+++ react-demo-019-redux-action-creator-thunk-demo/src/App.js
@@ -0,0 +1,49 @@
+import { useEffect, Fragment } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import Notification from "./components/UI/Notification";
+import { getCartData, sendCartData } from "./store/cart-actions";
+
+// firebase URL: https://react-http-backend-16718-default-rtdb.firebaseio.com/
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+  const { cartIsVisible, notification } = useSelector((state) => state.ui);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(getCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ react-demo-019-redux-action-creator-thunk-demo/src/components/Shop/ProductItem.js
@@ -0,0 +1,31 @@
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { cartActions } from "../../store/cart-slice";
+import { useDispatch } from "react-redux";
+
+const ProductItem = (props) => {
+  const dispatch = useDispatch();
+  const { id, title, price, description } = props;
+
+  const addItemToCartHandler = () => {
+    const item = { id, title, price };
+    dispatch(cartActions.addItemToCart(item));
+  };
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>${price.toFixed(2)}</div>
+        </header>
+        <p>{description}</p>
+        <div className={classes.actions}>
+          <button onClick={addItemToCartHandler}>Add to Cart</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ProductItem;
